bd8e169875b030509af0330c3e7a49aee41a24bf
davmarin <damarinma@gmail.com>
2025-03-02 16:48:59 +0100

    Delete puzzle1provisional2.py

--- puzzle1provisional2.py
+++
@@ -1,9 +0,0 @@
-import sys
-from RPLCD.i2c import CharLCD
-
-lcd = CharLCD('PCF8574', 0x27, cols=20, rows=4)
-
-user_text = sys.stdin.read().strip().split("\n")[:4]  #Llegir i dividir el text
-for i, line in enumerate(user_text):  #Escriure les linies a lLCD
-    lcd.cursor_pos = (i, 0)
-    lcd.write_string(line[:20].ljust(20)) #Fa falta?
